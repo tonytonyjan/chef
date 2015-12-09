f79f0187bcbf4082f016ed558bc587d8952969c9
Thom May <thom@chef.io>
2015-12-09 11:00:31 +0000

    switch synchronizer

--- lib/chef/cookbook/synchronizer.rb
+++ lib/chef/cookbook/synchronizer.rb
@@ -274,7 +274,7 @@ class Chef
     # downloaded to the path +destination+ which is relative to the Chef file
     # cache root.
     def download_file(url, destination)
-      raw_file = server_api.get_rest(url, true)
+      raw_file = server_api.streaming_request(url)
 
       Chef::Log.info("Storing updated #{destination} in the cache.")
       cache.move_to(raw_file.path, destination)
@@ -286,7 +286,7 @@ class Chef
     end
 
     def server_api
-      Chef::REST.new(Chef::Config[:chef_server_url])
+      Chef::ServerAPI.new(Chef::Config[:chef_server_url])
     end
 
   end
